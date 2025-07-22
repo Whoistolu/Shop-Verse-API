5e81641fe2a7284ea44fda8f58e6f69a25ecc34d
Whoistolu <digittolu25@gmail.com>
2025-07-22 12:04:18 +0100

    Seed default roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+UserRole.create(name: "super_admin", description: "Super Admin with all permissions")
+UserRole.create(name: "admin", description: "Brand owner")
+UserRole.create(name: "customer", description: "Standard Customer")
+UserRole.create(name: "guest", description: "Guest User with limited access")
