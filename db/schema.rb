9cc46a9e61c09dccccb18c2b5cb9d4735f7add69
Whoistolu <digittolu25@gmail.com>
2025-07-22 12:34:51 +0100

    Create products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_22_112418) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_22_113313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -49,6 +49,21 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_22_112418) do
     t.index ["customer_id"], name: "index_delivery_addresses_on_customer_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "stock"
+    t.integer "status"
+    t.string "image_url"
+    t.bigint "category_id", null: false
+    t.bigint "brand_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_products_on_brand_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "user_roles", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -75,5 +90,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_22_112418) do
   add_foreign_key "brands", "users"
   add_foreign_key "customers", "users"
   add_foreign_key "delivery_addresses", "customers"
+  add_foreign_key "products", "brands"
+  add_foreign_key "products", "categories"
   add_foreign_key "users", "user_roles"
 end
