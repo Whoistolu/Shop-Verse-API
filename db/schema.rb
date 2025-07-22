f595a2ffc380faf354d2e94bf955421c99f05aef
Whoistolu <digittolu25@gmail.com>
2025-07-22 12:25:05 +0100

    Generate categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_22_112142) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_22_112418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_22_112142) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_brands_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
