c86c1b550d7d39c9c60b4dd0772bc54dec2c3fe2
Whoistolu <digittolu25@gmail.com>
2025-07-23 23:27:03 +0100

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -299,12 +299,8 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
     jwt.dispatch_requests = [
-      [ "POST", %r{^/login$} ]
+      ["POST", %r{^/login$}]
     ]
-    jwt.revocation_requests = [
-      [ "DELETE", %r{^/logout$} ]
-    ]
-    jwt.revocation_strategy = JwtDenylist
   end
 
   # ==> Hotwire/Turbo configuration
