24e8df2ab7b0c44867912daa87ca5a9c64979fbd
Whoistolu <digittolu25@gmail.com>
2025-07-25 23:53:58 +0100

    Update devise.rb with the necessary routes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -299,7 +299,9 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
     jwt.dispatch_requests = [
-      [ "POST", %r{^/api/v1/users/sign_in$} ]
+      [ "POST", %r{^/api/v1/users/sign_in$} ],
+      [ "POST", %r{^/api/v1/auth/brand_signup$} ],
+      [ "POST", %r{^/api/v1/auth/customer_signup$} ]
     ]
   end
 
