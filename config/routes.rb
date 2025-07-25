7ed76124e8932c8f515f8f68f44d2efe8f42f8e4
Whoistolu <digittolu25@gmail.com>
2025-07-25 23:53:36 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-        devise_for :users,
-          path: "auth",
-          controllers: {
-            sessions: "api/v1/users/sessions",
-            registrations: "api/v1/users/registrations"
-          },
-          default: { format: :json }
+      # Devise routes
+      devise_for :users,
+        path: "auth",
+        controllers: {
+          sessions: "api/v1/users/sessions",
+          registrations: "api/v1/users/registrations"
+        },
+        default: { format: :json }
+
+      post "auth/brand_signup", to: "users/registrations#brand_signup"
+      post "auth/customer_signup", to: "users/registrations#customer_signup"
     end
   end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
