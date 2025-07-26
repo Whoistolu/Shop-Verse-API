ffc423d9c1d345cb28ee1580001579019c00ccf0
Whoistolu <digittolu25@gmail.com>
2025-07-26 01:26:56 +0100

    Update devise.rb with the recent routes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -299,9 +299,11 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
     jwt.dispatch_requests = [
-      [ "POST", %r{^/api/v1/users/sign_in$} ],
+      [ "POST", %r{^/api/v1/auth/sign_in$} ],
       [ "POST", %r{^/api/v1/auth/brand_signup$} ],
-      [ "POST", %r{^/api/v1/auth/customer_signup$} ]
+      [ "POST", %r{^/api/v1/auth/customer_signup$} ],
+      [ "POST", %r{^/api/v1/auth/brand_login$} ],
+      [ "POST", %r{^/api/v1/auth/customer_login$} ]
     ]
   end
 
