4d3ca182abfd1f5532630cbb5f2da2c58438c18f
Whoistolu <digittolu25@gmail.com>
2025-07-26 01:25:38 +0100

    Create Custom registrations Controller

---
+++ app/controllers/api/v1/users/custom_registrations_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::Users::CustomRegistrationsController < ApplicationController
+  respond_to :json
+  skip_before_action :authenticate_user!, only: [ :brand_signup, :customer_signup ]
+
+  def brand_signup
+    handle_signup("brand_owner", "Brand Owner Registration successful")
+  end
+
+  def customer_signup
+    handle_signup("customer", "Customer Registration successful")
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
+  end
+
+  def handle_signup(role_name, success_message)
+    user_role = UserRole.find_by(name: role_name)
+
+    unless user_role
+      render json: { error: "Role not found" }, status: :unprocessable_entity
+      return
+    end
+
+    user = User.new(sign_up_params.merge(user_role_id: user_role.id))
+
+    if user.save
+      token = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first
+
+      render json: {
+        message: success_message,
+        user: {
+          id: user.id,
+          email: user.email,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          role_id: user.user_role_id,
+          token: token
+        }
+      }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
