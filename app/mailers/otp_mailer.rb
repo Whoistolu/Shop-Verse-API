f9f53d400c2c67f456d80211e1762d3cfe8220c2
Whoistolu <digittolu25@gmail.com>
2025-07-31 13:52:28 +0100

    Refactor the OTP mailer

--- app/mailers/otp_mailer.rb
+++ app/mailers/otp_mailer.rb
@@ -1,9 +1,9 @@
 class OtpMailer < ApplicationMailer
     default from: "no-reply@shopverse.com"
 
-    def send_otp(otp, user)
-        @user = user
-        @otp = otp
+    def send_otp(user, otp)
+       @user = user
+       @otp = otp.code
         mail(to: @user.email, subject: "Your OTP Code")
     end
 end
