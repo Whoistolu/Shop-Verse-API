c0ad0d80220afaeaa2c694c91eded7485e873961
Whoistolu <digittolu25@gmail.com>
2025-07-31 13:58:28 +0100

    Remove unused directory

--- test/mailers/previews/user_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-class UserMailerPreview < ActionMailer::Preview
-  def otp_email
-    user = User.first || User.new(email: "demo@shopverse.com")
-    UserMailer.with(user: user, otp_code: "123456").otp_email
-  end
-end
