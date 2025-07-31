1306d4c3a9a838ce6749c78a80a77ff6f709108e
Whoistolu <digittolu25@gmail.com>
2025-07-31 13:52:45 +0100

    Update the OTP generator

--- app/services/otp_generator.rb
+++ app/services/otp_generator.rb
@@ -1,13 +1,14 @@
 class OtpGenerator
-  OTP_LENGTH = 6
-  OTP_EXPIRY = 10.minutes
+  EXPIRY = 10.minutes
 
   def self.generate_for(user)
-    code = rand.to_s[2..(1 + OTP_LENGTH)]
-    user.update!(
-      otp_code: code,
-      otp_generated_at: Time.current
+    code = rand.to_s[2..7]
+
+    otp = user.otps.create!(
+      code: code,
+      expires_at: Time.current + EXPIRY
     )
-    code
+
+    otp
   end
 end
