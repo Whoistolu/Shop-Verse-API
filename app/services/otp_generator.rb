799205eb53ff3bb1c7adde61606ae6a574c74a8e
Whoistolu <digittolu25@gmail.com>
2025-08-01 13:06:42 +0100

    Update the OTP generation to cryptographically secure method

--- app/services/otp_generator.rb
+++ app/services/otp_generator.rb
@@ -2,7 +2,7 @@ class OtpGenerator
   EXPIRY = 10.minutes
 
   def self.generate_for(user)
-    code = rand.to_s[2..7]
+    code = SecureRandom.random_number(1_000_000).to_s.rjust(6, "0")
 
     otp = user.otps.create!(
       code: code,
