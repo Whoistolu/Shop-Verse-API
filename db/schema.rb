c86e6080b1e7172df93e0b942f7961d221ea5a7b
Whoistolu <digittolu25@gmail.com>
2025-08-04 23:34:38 +0100

    Update the OTPS table with the used boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_31_093825) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_04_223152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -85,6 +85,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_31_093825) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "used"
     t.index ["user_id"], name: "index_otps_on_user_id"
   end
 
