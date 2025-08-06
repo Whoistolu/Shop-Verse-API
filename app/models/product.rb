be3f175bd56787873d2bf3c56dbdd970776b36d2
Whoistolu <digittolu25@gmail.com>
2025-08-06 23:53:19 +0100

    Add Product model validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
+  validates :name, :description, :price, :stock, :status, :image_url, :category_id, :brand_id, presence: true
 
   enum status: { published: 0, unpublished: 1 }
 end
