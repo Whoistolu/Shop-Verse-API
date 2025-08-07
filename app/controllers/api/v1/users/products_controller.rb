e3c178ddb5bc2bc6d3ebb5734aa29846ef0bd33e
Whoistolu <digittolu25@gmail.com>
2025-08-07 23:29:22 +0100

    Add set_product private method to prevent a brand owner from editing/deleting a product they don’t own ( for multi-tenant security)

--- app/controllers/api/v1/users/products_controller.rb
+++ app/controllers/api/v1/users/products_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ProductController < ApplicationController
     before_action :authenticate_user!
     before_action :ensure_brand_owner, only: [ :create, :index ]
+    before_action :set_product, only: [ :update, :destroy ]
+    respond_to :json
 
     def index
        @products = current_user.brand.products
@@ -38,5 +40,10 @@ class Api::V1::ProductController < ApplicationController
         unless current_user.user_role.name == "brand_owner"
             render json: { error: "You are not authorized to perform this action" }, status: :forbidden
         end
+    end
+
+    def set_product
+        @product = current_user.brand.products.find_by(id: params[:id])
+        render json: { error: "Product not found" }, status: :not_found unless @product
     end
 end
