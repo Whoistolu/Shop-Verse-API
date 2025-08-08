35ccde93d05c91dc7c602452936bca8db5faa2f3
Whoistolu <digittolu25@gmail.com>
2025-08-08 22:54:58 +0100

    Refactor Product controller to handle all other edge cases

--- app/controllers/api/v1/users/products_controller.rb
+++ app/controllers/api/v1/users/products_controller.rb
@@ -1,62 +1,69 @@
 class Api::V1::ProductController < ApplicationController
-    before_action :authenticate_user!
-    before_action :ensure_brand_owner, only: [ :create, :index ]
-    before_action :set_product, only: [ :update, :destroy, :show ]
-    respond_to :json
+  before_action :authenticate_user!
+  before_action :ensure_brand_owner, only: [:create, :index]
+  before_action :set_product, only: [:update, :destroy, :show]
+  respond_to :json
 
-    def index
-       @products = current_user.brand.products
+  def index
+    @products = current_user.brand.products
+    render json: @products
+  end
+
+  def create
+    return render json: { error: "Invalid category" }, status: :unprocessable_entity unless Category.exists?(params[:product][:category_id])
+
+    @product = current_user.brand.products.new(product_params.except(:brand_id))
+
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: { error: @product.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @product = current_user.brand.products.new(product_params)
+  def update
+    return render json: { error: "Invalid category" }, status: :unprocessable_entity unless Category.exists?(params[:product][:category_id])
+
+    if @product.update(product_params.except(:brand_id))
+      render json: { message: "Product updated successfully", product: @product }, status: :ok
+    else
+      render json: { error: @product.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def update
-        @product = current_user.brand.products.find(params[:id])
-        if @product.update(product_params)
-            render json: { message: "Product updated successfully" }, status: :ok
-        else
-            render json: { error: @product.errors.full_messages }, status: :unprocessable_entity
-        end
+  def destroy
+    if @product.destroy
+      render json: { message: "Product deleted successfully" }, status: :ok
+    else
+      render json: { error: @product.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def destroy
-        @product = current_user.brand.products.find(params[:id])
-        if @product.destroy
-            render json: { message: "Product deleted successfully" }, status: :ok
-        else
-            render json: { error: @product.errors.full_messages }, status: :unprocessable_entity
-        end
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :category_id, :status, :stock, :image_url)
+  end
+
+  def ensure_brand_owner
+    unless current_user.user_role.name == "brand_owner"
+      render json: { error: "You are not authorized to perform this action" }, status: :forbidden
     end
+  end
 
-    private
+  def set_product
+    @product = case current_user.user_role.name
+               when "brand_owner"
+                 current_user.brand.products.find_by(id: params[:id])
+               when "customer"
+                 Product
+                   .joins(:brand)
+                   .where(id: params[:id], status: "active", brands: { active: true })
+                   .first
+               when "super_admin"
+                 Product.find_by(id: params[:id])
+               end
 
-    def product_params
-         params.require(:product).permit(:name, :description, :price, :category_id, :status, :stock, :status, :image_url)
-    end
-
-    def ensure_brand_owner
-        unless current_user.user_role.name == "brand_owner"
-            render json: { error: "You are not authorized to perform this action" }, status: :forbidden
-        end
-    end
-
-    def set_product
-        case current_user.user_role.name
-        when "brand_owner"
-            @product = current_user.brand.products.find_by(id: params[:id])
-        when "customer"
-            @product = Product
-                        .joins(:brand)
-                        .where(id: params[:id], status: "active", brands: { active: true })
-                        .first
-        when "super_admin"
-            @product = Product.find_by(id: params[:id])
-        end
-
-        unless @product
-            render json: { error: "Product not found or not accessible" }, status: :not_found
-        end
-    end
+    render json: { error: "Product not found or not accessible" }, status: :not_found unless @product
+  end
 end
