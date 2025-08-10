38b2ead017e25bc2ee7745d75a3d6beb6f6e2176
Whoistolu <digittolu25@gmail.com>
2025-08-10 23:19:44 +0100

    Added authentication and authorization for super admin brand controller

--- app/controllers/api/v1/super_admin/brands_controller.rb
+++ app/controllers/api/v1/super_admin/brands_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::SuperAdmin::BrandsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :ensure_super_admin
+    respond_to :json
+
+    private
+
+    def ensure_super_admin
+        unless current_user.user_role.name == "super_admin"
+            render json: { error: "You are not authorized to perform this action" }, status: :forbidden
+        end
+    end
 end
