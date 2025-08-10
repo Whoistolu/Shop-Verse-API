4208b64613b9589595cda3fba982ca12a0472ba1
Whoistolu <digittolu25@gmail.com>
2025-08-10 23:32:49 +0100

    Fetch all brand-owner accounts

--- app/controllers/api/v1/super_admin/brands_controller.rb
+++ app/controllers/api/v1/super_admin/brands_controller.rb
@@ -3,6 +3,11 @@ class Api::V1::SuperAdmin::BrandsController < ApplicationController
     before_action :ensure_super_admin
     respond_to :json
 
+    def index
+        brands = User.includes(:user_role).where(user_roles: { name: "brand_owner" })
+        render json: brands, status: :ok
+    end
+
     private
 
     def ensure_super_admin
