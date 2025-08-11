36b9446805f58bfa77669fa8ec62e569232abeae
Whoistolu <digittolu25@gmail.com>
2025-08-11 20:38:33 +0100

    Add authentication and role check for super admin users in Api::V1::SuperAdmin::UsersController

--- app/controllers/api/v1/super_admin/users_controller.rb
+++ app/controllers/api/v1/super_admin/users_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::SuperAdmin::UsersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :ensure_super_admin
+
+    private
+
+    def ensure_super_admin
+        unless current_user.user_role.name == "super_admin"
+            render json: { error: "You are not authorized to perform this action" }, status: :forbidden
+        end
 end
