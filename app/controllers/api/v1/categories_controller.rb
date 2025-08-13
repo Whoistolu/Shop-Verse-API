435416a1f95e7df9131b91c285b39cd79de57e46
Whoistolu <digittolu25@gmail.com>
2025-08-13 22:47:06 +0100

    Add show action to display individual category

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,4 +3,11 @@ class Api::V1::CategoriesController < ApplicationController
         categories = Category.includes(:products)
         render json: categories, include: :products
     end
+
+    def show
+        category = Category.find(params[:id])
+        render json: category, include: :products
+    end
 end
+
+
