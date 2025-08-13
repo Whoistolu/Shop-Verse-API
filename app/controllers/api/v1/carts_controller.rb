5550873cb235008c6dbad3ffcaa80a46d7472225
Whoistolu <digittolu25@gmail.com>
2025-08-13 22:53:46 +0100

    Add show action to display logged in User's cart

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::CartsController < ApplicationController
     before_action :authenticate_user!
 
-    
+    def show
+        render json: current_user.cart, include: :cart_items
+    end
 end
