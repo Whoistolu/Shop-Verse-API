860cb91b1c43ec78e685a4aa6a439390882a9143
Whoistolu <digittolu25@gmail.com>
2025-08-13 22:45:59 +0100

    Add index action to list all categories

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::CategoriesController < ApplicationController
+    def index
+        categories = Category.includes(:products)
+        render json: categories, include: :products
+    end
 end
