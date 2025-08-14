fd77893e1c3d747859668ada544890bc34146815
Whoistolu <digittolu25@gmail.com>
2025-08-14 23:53:32 +0100

    Add add_item action to Api::V1::CartsController

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -4,4 +4,11 @@ class Api::V1::CartsController < ApplicationController
     def show
         render json: current_user.cart, include: :cart_items
     end
+
+    def add_item
+        cart = current_user.cart || current_user.create_cart
+        product = Product.find(params[:product_id])
+        cart.add_product(product, params[:quantity] || 1)
+        render json: cart, include: :cart_items
+    end
 end
