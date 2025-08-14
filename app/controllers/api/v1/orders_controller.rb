3c3d3b20dbbdefccb22b1cccf4949d6274628924
Whoistolu <digittolu25@gmail.com>
2025-08-15 00:00:49 +0100

    Add private method order_params to permit order item attributes

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -15,4 +15,10 @@ class Api::V1::OrdersController < ApplicationController
         order = current_user.orders.create(order_params)
         render json: order, status: :created
     end
+
+    private
+
+    def order_params
+        params.require(:order).permit(:shipping_address, :payment_method, order_items_attributes: [:product_id, :quantity])
+    end
 end
