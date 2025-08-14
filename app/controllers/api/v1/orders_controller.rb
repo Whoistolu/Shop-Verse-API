efa4e00efe8ffdb74e9ba08ff5fb3ba6762a9856
Whoistolu <digittolu25@gmail.com>
2025-08-14 23:59:05 +0100

    Addindex action to Api::V1::OrdersController to render current user's orders as JSON.

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,3 +1,8 @@
 class Api::V1::OrdersController < ApplicationController
      before_action :authenticate_user!
+
+     def index
+        orders = current_user.orders
+        render json: orders
+    end
 end
