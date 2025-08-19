b50c6c8da44a5ae544dc28a2c187e7c0a44db51a
Whoistolu <digittolu25@gmail.com>
2025-08-19 23:58:14 +0100

    Refactor Product controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::ProductController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_brand_owner!
+  validates :category_id, presence: true
+  validate :category_must_be_global
 
   def create
-  
     product = current_user.owned_brand.products.new(product_params)
 
     if product.save
@@ -18,10 +19,14 @@ class Api::V1::ProductController < ApplicationController
   def authorize_brand_owner!
     unless current_user.has_role?(:brand_owner)
       render json: { error: "Unauthorized" }, status: :unauthorized
-      return
+    end
   end
 
   def product_params
     params.require(:product).permit(:name, :description, :price, :stock, :status, :image_url, :category_id)
+  end
+
+  def category_must_be_global
+    raise "is invalid" unless Category.exists?(id: category_id)
   end
 end
