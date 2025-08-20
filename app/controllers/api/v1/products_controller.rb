ca774af947dadcf325e4e8a9d3e6e1e874544752
Whoistolu <digittolu25@gmail.com>
2025-08-20 23:56:44 +0100

    Refactor the Product controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ProductController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_brand_owner!
-  validates :category_id, presence: true
-  validate :category_must_be_global
+  before_action :set_product, only: [:update_stock]
+  
 
   def create
     product = current_user.owned_brand.products.new(product_params)
@@ -26,7 +26,7 @@ class Api::V1::ProductController < ApplicationController
 
   def authorize_brand_owner!
     unless current_user.has_role?(:brand_owner)
-      render json: { error: "Unauthorized" }, status: :unauthorized
+      render json: { error: "Unauthorized" }, status: :unauthorized and return
     end
   end
 
@@ -34,13 +34,15 @@ class Api::V1::ProductController < ApplicationController
     params.require(:product).permit(:name, :description, :price, :stock, :status, :image_url, :category_id)
   end
 
-  def category_must_be_global
-    raise "is invalid" unless Category.exists?(id: category_id)
-  end
+  
 
   def set_product
     @product = current_user.owned_brand.products.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render json: { error: "Product not found or not owned by your brand" }, status: :not_found
+  end
+
+  def stock_params
+    params.require(:product).permit(:stock)
   end
 end
