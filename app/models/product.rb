1112547d6a9c97e4627bbdfa4e6e5862e5277bf2
Whoistolu <digittolu25@gmail.com>
2025-08-20 23:59:22 +0100

    Set default status for products and scope the default-setting to only new records

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
+
+
   validates :name, :description, :price, :stock, :status, :image_url, :category_id, :brand_id, presence: true
   validates :category_id, presence: true
-  validate :category_must_be_global
+  validate :category_must_be_globalbefore_validation :set_default_status, on: :create
 
   def category_must_be_global
     unless Category.exists?(id: category_id)
@@ -12,4 +14,10 @@ class Product < ApplicationRecord
   end
 
   enum status: { published: 0, unpublished: 1 }
+
+  private
+
+  def set_default_status
+    self.status ||= :unpublished
+  end
 end
