7da31b227fdf7f5704f3ea21ace6e8f9bbe32761
Whoistolu <digittolu25@gmail.com>
2025-08-20 23:56:26 +0100

    Update the Product model with the necessaryy validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,14 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
   validates :name, :description, :price, :stock, :status, :image_url, :category_id, :brand_id, presence: true
+  validates :category_id, presence: true
+  validate :category_must_be_global
+
+  def category_must_be_global
+    unless Category.exists?(id: category_id)
+      errors.add(:category_id, "must be valid")
+    end
+  end
 
   enum status: { published: 0, unpublished: 1 }
 end
