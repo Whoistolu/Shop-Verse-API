31f7f9aaab2b5bd7d9fd3285860f29639e73dd4f
Whoistolu <digittolu25@gmail.com>
2025-08-21 23:52:03 +0100

    Update the status

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,14 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items
 
-  enum status: { pending: 0, processed: 1, shipped: 2, delivered: 3, cancelled: 4 }
+  enum status: {
+    pending: 0,
+    processed: 1,
+    partly_shipped: 2,
+    shipped: 3,
+    partly_delivered: 4,
+    delivered: 5,
+    cancelled: 6
+  }
+
 end
