59ae4982945f1170109c46ed25f20a312eac0d26
Whoistolu <digittolu25@gmail.com>
2025-08-22 23:17:29 +0100

    Add refresh_status method to the Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,4 +12,21 @@ class Order < ApplicationRecord
     cancelled: 6
   }
 
+  def refresh_status!
+    if order_items.all?(&:cancelled?)
+      update!(status: :cancelled)
+    elsif order_items.all?(&:delivered?)
+      update!(status: :delivered)
+    elsif order_items.all? { |item| item.shipped? || item.delivered? }
+      update!(status: :shipped)
+    elsif order_items.any?(&:shipped?)
+      update!(status: :partly_shipped)
+    elsif order_items.any?(&:delivered?)
+      update!(status: :partly_delivered)
+    elsif order_items.all?(&:processed?)
+      update!(status: :processed)
+    else
+      update!(status: :pending)
+    end
+  end
 end
