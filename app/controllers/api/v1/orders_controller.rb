bd1342045b8bea52487ef0b3d72c37f1c5af9830
Whoistolu <digittolu25@gmail.com>
2025-08-23 23:44:47 +0100

    Update the index action of the OrdersController

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,9 +1,15 @@
 class Api::V1::OrdersController < ApplicationController
      before_action :authenticate_user!
 
-     def index
-        orders = current_user.orders
-        render json: orders
+    def index
+        orders = Order.joins(:order_items)
+                    .where(order_items: { product_id: current_user.owned_brand.product_ids })
+                    .distinct
+
+        render json: orders.as_json(include: {
+        order_items: { include: :product },
+        customer: { only: [:id, :email] }
+        })
     end
 
      def show
