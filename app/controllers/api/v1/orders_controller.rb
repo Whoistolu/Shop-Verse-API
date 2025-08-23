891e7b1ed46d60e21bfd12ffe41bab2dca3a3118
Whoistolu <digittolu25@gmail.com>
2025-08-23 23:47:26 +0100

    Update the Order controller with private methods

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::OrdersController < ApplicationController
      before_action :authenticate_user!
+     before_action :authorize_brand_owner!
 
     def index
         orders = Order.joins(:order_items)
@@ -24,19 +25,9 @@ class Api::V1::OrdersController < ApplicationController
         end
     end
 
-     def show
-        order = current_user.orders.find(params[:id])
-        render json: order
-    end
-
-    def create
-        order = current_user.orders.create(order_params)
-        render json: order, status: :created
-    end
-
     private
 
-    def order_params
-        params.require(:order).permit(:shipping_address, :payment_method, order_items_attributes: [ :product_id, :quantity ])
+    def authorize_brand_owner!
+        render json: { error: "Unauthorized" }, status: :unauthorized unless current_user.has_role?(:brand_owner)
     end
 end
