3a252bc7cc49726e34621f9f9eb60a55f7d883b9
Whoistolu <digittolu25@gmail.com>
2025-09-05 23:51:48 +0100

    `Remove unused authentication and cart management methods from Api::V1::CartsController`

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -1,20 +1,2 @@
 class Api::V1::CartsController < ApplicationController
-    before_action :authenticate_user!
-
-    def show
-        render json: current_user.cart, include: :cart_items
-    end
-
-    def add_item
-        cart = current_user.cart || current_user.create_cart
-        product = Product.find(params[:product_id])
-        cart.add_product(product, params[:quantity] || 1)
-        render json: cart, include: :cart_items
-    end
-
-    def remove_item
-        cart = current_user.cart
-        cart.remove_product(params[:product_id])
-        render json: cart, include: :cart_items
-    end
 end
