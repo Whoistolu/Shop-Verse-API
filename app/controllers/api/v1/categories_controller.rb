c27aa688413fa72ac88b87c8dfb219ea62cc4603
Whoistolu <digittolu25@gmail.com>
2025-09-06 23:56:45 +0100

    `Add index and show actions to Api::V1::CategoriesController`

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::CategoriesController < ApplicationController
+    def index
+        categories = Category.includes(:products)
+        render json: categories, include: :products
+    end
+
+    def show
+        category = Category.find(params[:id])
+        render json: category, include: :products
+    end
 end
