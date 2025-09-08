b70ed8fb212cca67e78d7a6e4b0faddb60f9f022
Whoistolu <digittolu25@gmail.com>
2025-09-08 23:55:50 +0100

    Customer side of Orders listing

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::OrdersController < ApplicationController
+    #customer side
+    def index
+        orders = current_user.orders.includes(:order_items, :customer)
+        render json: orders.as_json(include: { order_items: { include: :product } })
+    end
 end
