61e8daa8468c403ff8ca1038630669109c426137
Whoistolu <digittolu25@gmail.com>
2025-09-08 23:56:38 +0100

    `Add show action to Api::V1::OrdersController`

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::OrdersController < ApplicationController
         orders = current_user.orders.includes(:order_items, :customer)
         render json: orders.as_json(include: { order_items: { include: :product } })
     end
+
+    def show
+        order = current_user.orders.find(params[:id])
+        render json: order.as_json(include: { order_items: { include: :product } })
+    end
 end
