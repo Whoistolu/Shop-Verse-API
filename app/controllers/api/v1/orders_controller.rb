631cb9447e09a2924783f5a5dec60a6714fa8c27
Whoistolu <digittolu25@gmail.com>
2025-09-09 23:51:14 +0100

    `Add index action to Api::V1::OrdersController to fetch user's orders with associated items and customer.`

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::OrdersController < ApplicationController
     #customer side
+    def index
+        orders = current_user.orders.includes(:order_items, :customer)
+        render json: orders.as_json(include: { order_items: { include: :product } })
+    end
+
     def show
         order = current_user.orders.find(params[:id])
         render json: order.as_json(include: { order_items: { include: :product } })
