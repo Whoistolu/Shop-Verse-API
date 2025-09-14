7a2cf672dde86db6957d41641d1a08b2e372a349
Whoistolu <digittolu25@gmail.com>
2025-09-14 23:49:06 +0100

    `Remove create action and related methods from Api::V1::OrdersController`

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -10,29 +10,5 @@ class Api::V1::OrdersController < ApplicationController
         render json: order.as_json(include: { order_items: { include: :product } })
     end
 
-    def create
-        order = current_user.orders.new(order_params)
-
-        if order.save
-        render json: order, status: :created
-        else
-        render json: { errors: order.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
-
-
-    private
-
-    def authorize_customer!
-        render json: { error: "Unauthorized" }, status: :unauthorized unless current_user.has_role?(:customer)
-    end
-
-    def order_params
-        params.require(:order).permit(
-        :delivery_address,
-        :delivery_phone_number,
-        :delivery_recipient_name,
-        order_items_attributes: [:product_id, :quantity]
-        )
-    end
+    
 end
