ce326786f57e9aa7f7214fc9a4160fee36f665dd
Whoistolu <digittolu25@gmail.com>
2025-09-14 23:49:41 +0100

    "Add create action to Api::V1::OrdersController, added order_params method and authorize_customer! method, modified index action to remove customer side functionality."

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,14 +1,31 @@
 class Api::V1::OrdersController < ApplicationController
     #customer side
-    def index
-        orders = current_user.orders.includes(:order_items, :customer)
-        render json: orders.as_json(include: { order_items: { include: :product } })
+   
+
+    def create
+        order = current_user.orders.new(order_params)
+
+        if order.save
+        render json: order, status: :created
+        else
+        render json: { errors: order.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
-    def show
-        order = current_user.orders.find(params[:id])
-        render json: order.as_json(include: { order_items: { include: :product } })
+
+    private
+
+    def authorize_customer!
+        render json: { error: "Unauthorized" }, status: :unauthorized unless current_user.has_role?(:customer)
+        
     end
 
-    
+    def order_params
+        params.require(:order).permit(
+        :delivery_address,
+        :delivery_phone_number,
+        :delivery_recipient_name,
+        order_items_attributes: [:product_id, :quantity]
+        )
+    end
 end
