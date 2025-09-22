521a44f892f5267c2675949c127d543efc459774
Whoistolu <digittolu25@gmail.com>
2025-09-22 23:31:18 +0100

    Update the application controller with authenticate_user and current_uuser methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,19 @@ class ApplicationController < ActionController::API
     include Devise::Controllers::Helpers
     include Pundit::Authorization
 
-    before_action :authenticate_user!
-
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
     private
+
+    def authenticate_user!
+        unless warden.authenticate(scope: :user)
+            render json: { error: "Unauthorized" }, status: :unauthorized
+        end
+    end
+
+    def current_user
+        @current_user ||= warden.user(:user)
+    end
 
     def user_not_authorized(exception)
         render json: { error: "You are not authorized to perform this action." }, status: :forbidden
