65b1dec42fc21cdda5f1586067332ffa714ddd9e
Whoistolu <digittolu25@gmail.com>
2025-10-22 23:40:01 +0100

    `Add role_id attribute to UserSerializer`

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,14 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :user_role_id
+  attributes :id, :email, :first_name, :last_name, :user_role_id, :role_id
 
   # Include token only when it's available (during login)
   attribute :token, if: -> { object.instance_variable_get(:@token).present? }
 
   def token
     object.instance_variable_get(:@token)
+  end
+
+  def role_id
+    object.user_role_id
   end
 end
