1bc0f8f577c3256ad1affe1fe01c51fb6fb5f7f4
Whoistolu <digittolu25@gmail.com>
2025-11-08 23:58:56 +0100

    Remove services and policies

--- app/serializers/category_serializer.rb
+++
@@ -1,5 +0,0 @@
-class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name, :created_at, :updated_at
-
-  has_many :products
-end
--- app/serializers/product_serializer.rb
+++
@@ -1,6 +0,0 @@
-class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :stock, :image_url, :status, :created_at, :updated_at
-
-  belongs_to :brand
-  belongs_to :category
-end
