9cdd449070e82352cde480bed6145d6d7ab2e089
Whoistolu <digittolu25@gmail.com>
2025-11-08 23:49:11 +0100

    Refactor OTP model

--- app/models/otp.rb
+++ app/models/otp.rb
@@ -1,14 +1,3 @@
 class Otp < ApplicationRecord
-  belongs_to :user
-
-  validates :code, presence: true, uniqueness: true
-  validates :expires_at, presence: true
-
-  def expired?
-    Time.current > expires_at
-  end
-
-  def mark_as_used
-    update!(used: true)
-  end
+  
 end
