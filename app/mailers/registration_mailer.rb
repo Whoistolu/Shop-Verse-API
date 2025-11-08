6c847713dbc61ca123f1cb8be111e776330d2854
Whoistolu <digittolu25@gmail.com>
2025-11-08 23:59:16 +0100

    Remove mailers

--- app/mailers/registration_mailer.rb
+++
@@ -1,8 +0,0 @@
-class RegistrationMailer < ApplicationMailer
-  default from: "no-reply@shopverse.com"
-
-  def pending_approval(user)
-    @user = user
-    mail(to: @user.email, subject: "Your registration is pending approval")
-  end
-end
