5f07a62bb0fc504c06f39466c031bca2cbdae754
Whoistolu <digittolu25@gmail.com>
2025-11-08 23:48:43 +0100

    Refactor Order Item model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,14 +1,3 @@
 class OrderItem < ApplicationRecord
-  belongs_to :order
-  belongs_to :product
-
-  enum status: { pending: 0,  processed: 1, shipped: 2, delivered: 3, cancelled: 4 }
-
-   after_update :update_order_status
-
-  private
-
-  def update_order_status
-    order.refresh_status!
-  end
+  
 end
