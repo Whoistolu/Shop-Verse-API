13f6b4eaea3bc94d68331371d62605ce11c9bd78
Whoistolu <digittolu25@gmail.com>
2025-11-12 23:59:17 +0100

    Add default FactoryBot factory for User model

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    email { "MyString" }
+    password_digest { "MyString" }
+    status { "MyString" }
+    user_role { nil }
+  end
+end
