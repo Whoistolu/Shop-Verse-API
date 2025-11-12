144fe63c320aa78c4114a45472f5172e0f2cf342
Whoistolu <digittolu25@gmail.com>
2025-11-12 23:58:32 +0100

    Add associations, enums, and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   belongs_to :user_role
+  has_secure_password
+
+  enum status: { pending: "pending", active: "active", inactive: "inactive" }
+
+  validates :first_name, :last_name, :email, :status, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
