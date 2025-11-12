25f059c6a6b9856a66f262d3dd68e4c60acdf894
Whoistolu <digittolu25@gmail.com>
2025-11-12 23:57:08 +0100

    Add constraints, default status, and unique email index to users table migration

---
+++ db/migrate/20251112225609_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false, index: { unique: true }
+      t.string :password_digest, null: false
+      t.string :status, null: false, default: "pending"
+      t.references :user_role, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
