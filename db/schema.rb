d94b9226e8769fcedee55216f594b625c2b56bb7
Whoistolu <digittolu25@gmail.com>
2025-11-12 23:57:37 +0100

    Migrate database to create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_12_224941) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_12_225609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,4 +27,18 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_12_224941) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "status", default: "pending", null: false
+    t.bigint "user_role_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["user_role_id"], name: "index_users_on_user_role_id"
+  end
+
+  add_foreign_key "users", "user_roles"
 end
