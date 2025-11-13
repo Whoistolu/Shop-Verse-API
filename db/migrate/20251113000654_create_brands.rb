c5ba8029e12b89fdc0fc836b50846a1b290450b4
Whoistolu <digittolu25@gmail.com>
2025-11-13 01:07:58 +0100

    Add not-null constraints and foreign key to brands table migration

---
+++ db/migrate/20251113000654_create_brands.rb
@@ -0,0 +1,11 @@
+class CreateBrands < ActiveRecord::Migration[7.1]
+  def change
+    create_table :brands do |t|
+      t.string :name, null: false
+      t.references :user, null: false, foreign_key: true
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
