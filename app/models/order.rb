f5af93b1552e3e7b922674134bde43139baed6da
Whoistolu <digittolu25@gmail.com>
2025-11-13 01:21:47 +0100

    Add associations, enums, and validations to Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :customer
+
+  enum status: {
+    pending: "pending",
+    processing: "processing",
+    partially_shipped: "partially_shipped",
+    shipped: "shipped",
+    partially_delivered: "partially_delivered",
+    delivered: "delivered",
+    cancelled: "cancelled"
+  }
+
+   validates :customer_id, :total_price, :status, :delivery_address, :delivery_phone_number, :delivery_recipient_name, presence: true
+  validates :total_price, numericality: { greater_than_or_equal_to: 0 }
 end
