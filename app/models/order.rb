45dd15c7941d9b9b13b485c922c92a488e55d723
Whoistolu <digittolu25@gmail.com>
2025-12-10 04:18:44 +0100

    Update the Order table association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
-  belongs_to :customer
+  belongs_to :user
+  has_many :order_items, dependent: :destroy
 
   enum status: {
     pending: "pending",
@@ -11,6 +12,6 @@ class Order < ApplicationRecord
     cancelled: "cancelled"
   }
 
-   validates :customer_id, :total_price, :status, :delivery_address, :delivery_phone_number, :delivery_recipient_name, presence: true
+  validates :user_id, :total_price, :status, :delivery_address, :delivery_phone_number, :delivery_recipient_name, presence: true
   validates :total_price, numericality: { greater_than_or_equal_to: 0 }
 end
