b04d66268399141b93759600a04ff2cb7f1fd21b
Whoistolu <digittolu25@gmail.com>
2025-12-14 21:25:16 +0100

    Convert existing User model to Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   belongs_to :user_role
   has_many :delivery_addresses
   has_many :orders
