cbc25815ace7c027d915e5985ff53702b72e878c
Whoistolu <digittolu25@gmail.com>
2025-12-14 21:23:31 +0100

    Remove PasswordDigest From Users Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_10_030624) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_14_202151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,7 +89,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_10_030624) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "email", null: false
-    t.string "password_digest", null: false
     t.string "status", default: "pending", null: false
     t.bigint "user_role_id", null: false
     t.datetime "created_at", null: false
